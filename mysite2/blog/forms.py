c7af8e298c6f2dced37cbda04f66c10d6a54ae30
Sh33v <philiprijn@gmail.com>
2019-06-02 01:44:15 +0200

    first change

--- mysite2/blog/forms.py
+++ mysite2/blog/forms.py
@@ -5,23 +5,21 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('author','title','text')
+        fields = ('author','title', 'text',)
 
-        widget = {
+        widgets = {
             'title': forms.TextInput(attrs={'class': 'textinputclass'}),
-            'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea postcontent'})
+            'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea postcontent'}),
         }
-
-
 
 
 class CommentForm(forms.ModelForm):
 
     class Meta:
         model = Comment
-        fields = ('author','text')
+        fields = ('author', 'text',)
 
         widgets = {
             'author': forms.TextInput(attrs={'class': 'textinputclass'}),
-            'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea'})
+            'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea'}),
         }
